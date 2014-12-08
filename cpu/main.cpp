b6f42f363b1873bc7948b8f73cf05bf605dcf1b5
Eric Wang <ericdwang@tbp.berkeley.edu>
2014-12-07 16:39:16 -0800

    Add minimum characters for each type and fix hash checking by not casting to signed chars

--- cpu/main.cpp
+++ cpu/main.cpp
@@ -13,13 +13,19 @@
 const int LOWERCASE_START = 97;
 const int UPPERCASE_START = 65;
 const int DIGIT_START = 48;
-const char PUNCTUATION[] = " !@#$%^&*()-_+=[{]}\\|;:'\",<.>/?`~";
+const int PUNCTUATION_START = 32;
 
-// Sets of characters
+// Whether the password contains these types of characters
 int lowercase = 0;
 int uppercase = 0;
 int digits = 0;
 int punctuation = 0;
+
+// Minimum number of characters for each type
+int min_lowercase = 0;
+int min_uppercase = 0;
+int min_digits = 0;
+int min_punctuation = 0;
 
 // The possible values that the password can take on
 char* values;
@@ -45,7 +51,7 @@ double timestamp() {
 /**
  * Raise a base to a power.
  */
-unsigned long long ipow(unsigned long long base, int exp) {
+unsigned long long ipow(int base, int exp) {
     unsigned long long result = 1ULL;
     while (exp) {
         if (exp & 1) {
@@ -82,8 +88,8 @@ void get_possible_values() {
         }
     }
     if (punctuation) {
-        for (i = 0; i < 33; i++) {
-            values[index] = PUNCTUATION[i];
+        for (i = PUNCTUATION_START; i < PUNCTUATION_START + 16; i++) {
+            values[index] = i;
             index++;
         }
     }
@@ -101,6 +107,34 @@ void get_guess(char guess[], unsigned long long index, int length) {
 }
 
 /**
+ * Check whether a password is valid according to the minimum number of
+ * characters for each type.
+ */
+int valid_guess(char guess[], int length) {
+    int num_lowercase = 0;
+    int num_uppercase = 0;
+    int num_digits = 0;
+    int num_punctuation = 0;
+
+    int i;
+    for (i = 0; i < length; i++) {
+        int curr = guess[i];
+        num_lowercase += (curr >= LOWERCASE_START &&
+                curr < LOWERCASE_START + 26);
+        num_uppercase += (curr >= UPPERCASE_START &&
+                curr < UPPERCASE_START + 26);
+        num_digits += (curr >= DIGIT_START &&
+                curr < DIGIT_START + 10);
+        num_punctuation += (curr >= PUNCTUATION_START &&
+                curr < PUNCTUATION_START + 16);
+    }
+    return (num_lowercase >= min_lowercase &&
+            num_uppercase >= min_uppercase &&
+            num_digits >= min_digits &&
+            num_punctuation >= min_punctuation);
+}
+
+/**
  * Bruteforce the string that results in the specified hash given the values
  * it can take on.
  */
@@ -108,10 +142,7 @@ void brute_force() {
     int length;
     int found = 0;
 
-    for (length = min_length; length <= max_length; length++) {
-        if (found == 1) {
-            break;
-        }
+    for (length = min_length; length <= max_length && !found; length++) {
         unsigned long long max_guesses = ipow(num_values, length);
         unsigned char buffer[SHA256_DIGEST_LENGTH];
         char guess[length + 1];
@@ -119,12 +150,15 @@ void brute_force() {
 
         # pragma omp parallel for private(buffer) private(guess)
         for (unsigned long long index = 0; index < max_guesses; index++) {
-            if (found == 1) {
+            if (found) {
                 continue;
             }
             get_guess(guess, index, length);
+            if (!valid_guess(guess, length)) {
+                continue;
+            }
             sha256(guess, buffer);
-            if (strncmp((char*) buffer, (char*) hash, SHA256_DIGEST_LENGTH) == 0) {
+            if (memcmp(buffer, hash, SHA256_DIGEST_LENGTH) == 0) {
                 memcpy(password, guess, length + 1);
                 found = 1;
             }
@@ -139,7 +173,7 @@ int main (int argc, char **argv) {
 
     // Parse arguments
     int c;
-    while((c = getopt(argc, argv, "h:m:n:ludp")) != -1) {
+    while((c = getopt(argc, argv, "h:m:n:l:u:d:p:")) != -1) {
         switch(c) {
             case 'h':
                 // Convert the hexidecimal string into a byte array
@@ -157,42 +191,47 @@ int main (int argc, char **argv) {
                 max_length = atoi(optarg);
                 break;
             case 'l':
+                min_lowercase = atoi(optarg);
                 lowercase = 1;
                 break;
             case 'u':
+                min_uppercase = atoi(optarg);
                 uppercase = 1;
                 break;
             case 'd':
+                min_digits = atoi(optarg);
                 digits = 1;
                 break;
             case 'p':
+                min_punctuation = atoi(optarg);
                 punctuation = 1;
                 break;
         }
     }
 
     // Check arguments
-    if (hash_arg == 0) {
+    if (!hash_arg) {
         printf("Usage: ./main\n"
-               "REQUIRED: -h (password hash)\n"
-               "OPTIONAL (default 1-5 characters, alphanumeric with capital letters):\n"
-               "-m (min password length) "
-               "-n (max password length) "
-               "-l (contains lowercase) "
-               "-u (contains uppercase) "
-               "-d (contains digits) "
-               "-p (contains punctuation) "
-               "\n");
+                "REQUIRED: -h (password hash)\n"
+                "OPTIONAL (default 1-5 characters, all possible characters, no minimum):\n"
+                "-m (min password length)\n"
+                "-n (max password length)\n"
+                "-l (min lowercase characters)\n"
+                "-u (min uppercase characters)\n"
+                "-d (min digits characters)\n"
+                "-p (min punctuation characters)\n"
+                );
         return 1;
     }
-    if (lowercase == 0 && uppercase == 0 && digits == 0 && punctuation == 0) {
+    if (!lowercase && !uppercase && !digits && !punctuation) {
         lowercase = 1;
         uppercase = 1;
         digits = 1;
+        punctuation = 1;
     }
 
     // Get the possible characters for the password
-    num_values = lowercase * 26 + uppercase * 26 + digits * 10 + punctuation * 35;
+    num_values = lowercase * 26 + uppercase * 26 + digits * 10 + punctuation * 16;
     values = (char*) malloc((num_values + 1) * sizeof(char));
     get_possible_values();
     values[num_values] = '\0';
@@ -200,9 +239,30 @@ int main (int argc, char **argv) {
     // Create guess buffer
     password = (char*) malloc((max_length + 1) * sizeof(char));
 
+    printf("Cracking password with %d to %d characters containing:\n",
+            min_length, max_length);
+    if (lowercase) {
+        printf("At least %d lowercase characters\n", min_lowercase);
+    } else {
+        printf("No lowercase characters\n");
+    }
+    if (uppercase) {
+        printf("At least %d uppercase characters\n", min_uppercase);
+    } else {
+        printf("No uppercase characters\n");
+    }
+    if (digits) {
+        printf("At least %d digits characters\n", min_digits);
+    } else {
+        printf("No digits characters\n");
+    }
+    if (punctuation) {
+        printf("At least %d punctuation characters\n", min_punctuation);
+    } else {
+        printf("No punctuation characters\n");
+    }
+
     // Bruteforce the password and check how long it takes
-    printf("Cracking password with a length of %d to %d characters containing "
-           "\"%s\"\n", min_length, max_length, values);
     double t0 = timestamp();
     brute_force();
     t0 = timestamp() - t0;
